7adb6d1e72b67660f3a2abe8ed9c61266fd71e30
BlairWachira <blairwachira@proton.me>
2026-03-28 11:16:53 +0000

    file arranger 2.0

--- filearrange.py
+++ filearrange.py
@@ -1,19 +1,51 @@
 import os
 import shutil
 
-os.chdir('C:/Users/user/OneDrive/Desktop/test/')
+import os
+
+mydir = input("Enter your directory path: ").strip()
+
+if os.path.isdir(mydir):
+    try:
+        os.chdir(mydir)
+        print("Changed directory successfully")
+    except PermissionError:
+        print("Permission denied")
+else:
+    print("Invalid directory")
 
 folders={
   ".mp3": "music",
+  ".mkv": "videos",
+  ".avi": "videos",
+  ".mov": "videos",
+  ".wmv": "videos",
+  ".flv": "videos",
   ".mp4": "videos",
+  ".jpg": "images",
+  ".jpeg": "images",
+  ".png": "images",
+  ".gif": "images",
+  ".bmp": "images",
+  ".webp": "images",
+  ".svg": "images",
   ".txt": "files",
-  ".pdf": "PDF_Documents"
+  ".pdf": "documents",
+  ".exe": "applications",
+  ".zip": "archives",
+  ".doc": "documents",
+  ".docx": "documents",
+  ".ppt": "presentations",
+  ".pptx": "presentations",
+  ".xls": "spreadsheets",
+  ".xlsx": "spreadsheets",
+  ".py": "programs"
 }
 
 for file in os.listdir():
   if os.path.isfile(file):
-    print("file exists")
     name , ext = os.path.splitext(file)
+    ext = ext.lower()
 
     if ext in folders:
       folder=folders[ext]
@@ -21,3 +53,4 @@ for file in os.listdir():
       os.makedirs(folder,exist_ok=True)
 
       shutil.move(file,folder)
+      print("changed sucessfully")
